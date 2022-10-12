03be8fc281df85b104a5e9ec3072803e795f8139
Rosehildson <98069464+Rosehildson@users.noreply.github.com>
2022-10-12 17:02:45 -0300

    Banco de dados H2
    criação do banco de dados apartir do mapeamento relacional do JPA

--- SpringNA/ApiUdemy/src/main/java/com/hildson/ApiUdemy/domain/Categoria.java
+++ SpringNA/ApiUdemy/src/main/java/com/hildson/ApiUdemy/domain/Categoria.java
@@ -3,11 +3,18 @@ package com.hildson.ApiUdemy.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-public class Categoria implements Serializable {
-	private static final long serialVersionUID = 1L;
+@Entity
+		public class Categoria implements Serializable {	
+			private static final long serialVersionUID = 1L;
 	
 /* === INCLUIDOS OS ATRIBUTOS BASICOS DA CLASSE === */
+@Id
+@GeneratedValue(strategy=GenerationType.IDENTITY)
 		private Integer id;
 		private String nome;
 		
